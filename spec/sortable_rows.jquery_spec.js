c5c0ef5f90c0674c723f752b07b382f5761f91a3
aalvarado <adan.alvarado7@gmail.com>
2015-01-27 22:07:01 -0800

    Test for sort on startup

--- spec/sortable_rows.jquery_spec.js
+++ spec/sortable_rows.jquery_spec.js
@@ -6,8 +6,19 @@ describe("sortableRows", function() {
     elem = $('table.sortable');
   });
 
-  it("is able to load plugin on table elements", function(){
-    $(elem).sortableRows();
-    expect($(elem).data('plugin_sortableRows')).not.toBe(null);
+  describe('.sortableRows()', function(){
+    it("is able to load plugin on table elements", function(){
+      $(elem).sortableRows();
+      expect($(elem).data('plugin_sortableRows')).not.toBe(null);
+    });
+
+    it("sorts on start", function() {
+      var first_row_el = 'tbody tr:first input.sort';
+      var first_row = $(elem).find( first_row_el );
+      expect( first_row.val() ).not.toBe('0');
+      $(elem).sortableRows();
+      first_row = $(elem).find( first_row_el );
+      expect( first_row.val() ).toBe('0');
+    });
   });
 });
