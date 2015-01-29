6693ba9fe0ffb18eb9a76bf838341d57b24a0769
aalvarado <adan.alvarado7@gmail.com>
2015-01-28 21:17:56 -0800

    Change jasmine test name

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jasmine: {
-      pivotal: {
+      tests: {
         src: 'src/*.js',
         options: {
           specs: 'spec/*_spec.js',
